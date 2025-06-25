7b176a12c4e29ffef255037835071c61cd089c9d
Daniel Noland <daniel@githedgehog.com>
2025-06-25 13:50:43 -0600

    feat: add support for parent dev name
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- src/link/attribute.rs
+++ src/link/attribute.rs
@@ -94,9 +94,9 @@ const IFLA_MAX_MTU: u16 = 51;
 const IFLA_PROP_LIST: u16 = 52;
 const IFLA_PERM_ADDRESS: u16 = 54;
 const IFLA_PROTO_DOWN_REASON: u16 = 55;
+const IFLA_PARENT_DEV_NAME: u16 = 56;
 
 /* TODO:(Gris Ge)
-const IFLA_PARENT_DEV_NAME: u16 = 56;
 const IFLA_PARENT_DEV_BUS_NAME: u16 = 57;
 const IFLA_GRO_MAX_SIZE: u16 = 58;
 const IFLA_TSO_MAX_SIZE: u16 = 59;
@@ -168,6 +168,7 @@ pub enum LinkAttribute {
     AfSpecUnspec(Vec<AfSpecUnspec>),
     AfSpecBridge(Vec<AfSpecBridge>),
     AfSpecUnknown(Vec<u8>),
+    ParentDevName(String),
     Other(DefaultNla),
 }
 
@@ -191,6 +192,7 @@ impl Nla for LinkAttribute {
             | Self::AfSpecUnknown(bytes) => bytes.len(),
 
             Self::IfName(string)
+            | Self::ParentDevName(string)
             | Self::Qdisc(string)
             | Self::IfAlias(string)
             | Self::PhysPortName(string) => string.len() + 1,
@@ -256,6 +258,7 @@ impl Nla for LinkAttribute {
             }
 
             Self::IfName(string)
+            | Self::ParentDevName(string)
             | Self::Qdisc(string)
             | Self::IfAlias(string)
             | Self::PhysPortName(string) => {
@@ -336,6 +339,7 @@ impl Nla for LinkAttribute {
             Self::Qdisc(_) => IFLA_QDISC,
             Self::IfAlias(_) => IFLA_IFALIAS,
             Self::PhysPortName(_) => IFLA_PHYS_PORT_NAME,
+            Self::ParentDevName(_) => IFLA_PARENT_DEV_NAME,
             Self::Mode(_) => IFLA_LINKMODE,
             Self::Carrier(_) => IFLA_CARRIER,
             Self::ProtoDown(_) => IFLA_PROTO_DOWN,
@@ -530,7 +534,11 @@ impl<'a, T: AsRef<[u8]> + ?Sized>
                 parse_string(payload)
                     .context("invalid IFLA_PHYS_PORT_NAME value")?,
             ),
-            IFLA_LINKMODE => Self::Mode(LinkMode::from(
+            IFLA_PARENT_DEV_NAME => Self::ParentDevName(
+                parse_string(payload)
+                    .context("invalid IFLA_PARENT_DEV_NAME value")?,
+            ),
+            IFLA_LINKMODE => Self::Mode(
                 parse_u8(payload).context("invalid IFLA_LINKMODE value")?,
             )),
             IFLA_CARRIER => Self::Carrier(
